333d156a321b1b666e09d2ce182f1336a7415a71
shinshi-onigoori <cantoca0702@gmail.com>
2023-05-10 22:37:31 +0900

    表部分をコンポーネントに抽出

--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from "styled-components";
 import { SAMPLE_HOLDING_STOCKS } from '../types/top';
 // import './top.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBackward, faForward } from '@fortawesome/free-solid-svg-icons'
+import StockTable from './components/stockTable';
 
 const TopContainer = styled.div`
     width: 100%;
@@ -62,103 +63,6 @@ const TableSearchButton = styled.div`
         :hover{
             background-color: #38514D;
         }
-    }
-`;
-
-const TableBack = styled.table`
-    width: auto;
-    margin: auto;
-    /* padding: 10px; */
-    box-sizing: border-box;
-    overflow: hidden;
-    border-spacing: 0px;
-    border: 1px solid #D1D1D1;
-    border-radius: 20px;
-    th{
-        max-width: 140px;
-        min-width: 115px;
-        height: 50px;
-    }
-`;
-
-const TableHead = styled.thead`
-      color: #fff;
-      th{
-        background-color: #4ADAE5;
-      }
-`;
-
-const TableBoby = styled.tbody`
-      color: #707070;
-`;
-
-const Code = styled.th`
-    background-color: #fff;
-`;
-
-const Name = styled.th`
-    background-color: #F5F5F5;
-`;
-
-const Close = styled.th`
-    background-color: #fff;
-`;
-
-const Yield = styled.th`
-    background-color: #F5F5F5;
-`;
-
-const CloseRatioFromPreviuseDay = styled.th`
-    background-color: #fff;
-`;
-
-const CloseRatioFromPurchaseDay = styled.th`
-    background-color: #F5F5F5;
-`;
-
-const HoldingNumber = styled.th`
-    background-color: #fff;
-`;
-
-const TotalAssetPlice = styled.th`
-    background-color: #F5F5F5;
-`;
-
-const TableBodyRow1 = styled.tr``;
-
-const TableBodyRow2 = styled.tr`
-    th{
-        border-top: 0.25px solid #D1D1D1;
-    }
-`;
-const TableBodyRow3 = styled.tr`
-    th{
-        border-top: 0.25px solid #D1D1D1;
-    }
-`;
-
-const TableBodyRow4 = styled.tr`
-    th{
-        border-top: 0.25px solid #D1D1D1;
-    }
-`;
-
-const TableBodyRow5 = styled.tr`
-    th{
-        border-top: 0.25px solid #D1D1D1;
-    }
-`;
-
-const TableBodyRow6 = styled.tr`
-    th{
-        border-top: 0.25px solid #D1D1D1;
-    }
-
-    Code{
-        border-bottom-left-radius: 20px;
-    }
-    TotalAssetPlice{
-        border-bottom-right-radius: 20px;
     }
 `;
 
@@ -233,6 +137,11 @@ const TopCopyright = styled.section`
 `;
 
 function Top() {
+
+    const tableProp = {
+        holdingStocks: SAMPLE_HOLDING_STOCKS
+    }
+    
     return (
         <TopContainer>
             <HeaderBack></HeaderBack>
@@ -243,85 +152,10 @@ function Top() {
                             <input type="search" placeholder="銘柄コードを検索" />
                         </TableSearchBox>
                         <TableSearchButton>
-                            <button>検索</button>
+                            <button onClick={() => console.log("search")}>検索</button>
                         </TableSearchButton>
                     </TableSearchBack>
-                    <TableBack>
-                        <TableHead>
-                            <tr>
-                                <th>銘柄コード</th>
-                                <th>名称</th>
-                                <th>終値</th>
-                                <th>利回り</th>
-                                <th>前日比</th>
-                                <th>購入時比</th>
-                                <th>銘柄保有数</th>
-                                <th>総額</th>
-                            </tr>
-                        </TableHead>
-                        <TableBoby>
-                            <TableBodyRow1>
-                                <Code></Code>
-                                <Name></Name>
-                                <Close></Close>
-                                <Yield></Yield>
-                                <CloseRatioFromPreviuseDay></CloseRatioFromPreviuseDay>
-                                <CloseRatioFromPurchaseDay></CloseRatioFromPurchaseDay>
-                                <HoldingNumber></HoldingNumber>
-                                <TotalAssetPlice></TotalAssetPlice>
-                            </TableBodyRow1>
-                            <TableBodyRow2>
-                                <Code></Code>
-                                <Name></Name>
-                                <Close></Close>
-                                <Yield></Yield>
-                                <CloseRatioFromPreviuseDay></CloseRatioFromPreviuseDay>
-                                <CloseRatioFromPurchaseDay></CloseRatioFromPurchaseDay>
-                                <HoldingNumber></HoldingNumber>
-                                <TotalAssetPlice></TotalAssetPlice>
-                            </TableBodyRow2>
-                            <TableBodyRow3>
-                                <Code></Code>
-                                <Name></Name>
-                                <Close></Close>
-                                <Yield></Yield>
-                                <CloseRatioFromPreviuseDay></CloseRatioFromPreviuseDay>
-                                <CloseRatioFromPurchaseDay></CloseRatioFromPurchaseDay>
-                                <HoldingNumber></HoldingNumber>
-                                <TotalAssetPlice></TotalAssetPlice>
-                            </TableBodyRow3>
-                            <TableBodyRow4>
-                                <Code></Code>
-                                <Name></Name>
-                                <Close></Close>
-                                <Yield></Yield>
-                                <CloseRatioFromPreviuseDay></CloseRatioFromPreviuseDay>
-                                <CloseRatioFromPurchaseDay></CloseRatioFromPurchaseDay>
-                                <HoldingNumber></HoldingNumber>
-                                <TotalAssetPlice></TotalAssetPlice>
-                            </TableBodyRow4>
-                            <TableBodyRow5>
-                                <Code></Code>
-                                <Name></Name>
-                                <Close></Close>
-                                <Yield></Yield>
-                                <CloseRatioFromPreviuseDay></CloseRatioFromPreviuseDay>
-                                <CloseRatioFromPurchaseDay></CloseRatioFromPurchaseDay>
-                                <HoldingNumber></HoldingNumber>
-                                <TotalAssetPlice></TotalAssetPlice>
-                            </TableBodyRow5>
-                            <TableBodyRow6>
-                                <Code></Code>
-                                <Name></Name>
-                                <Close></Close>
-                                <Yield></Yield>
-                                <CloseRatioFromPreviuseDay></CloseRatioFromPreviuseDay>
-                                <CloseRatioFromPurchaseDay></CloseRatioFromPurchaseDay>
-                                <HoldingNumber></HoldingNumber>
-                                <TotalAssetPlice></TotalAssetPlice>
-                            </TableBodyRow6>
-                        </TableBoby>
-                    </TableBack>
+                    <StockTable {...tableProp}></StockTable>
                     <TableButtonBack>
                         <ButtonLeftBack>
                             <LeftPagingButton onClick={() => console.log("back")}>
