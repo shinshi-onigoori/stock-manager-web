3136f904ce4e26ce34968084eec855088f1929e4
shinshi-onigoori <cantoca0702@gmail.com>
2023-05-22 00:55:32 +0900

    Rooting初期値の再整理

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Signin from './pages/signin';
 import reportWebVitals from './reportWebVitals';
-import Top from './pages/top';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Top />
+    <App />
   </React.StrictMode>
 );
 
